ca02568678e480dc914e9d533e8937dfa2411911
云朔 <yunshuo.zk@alibaba-inc.com>
2017-12-05 12:24:48 +0800

    change

--- index.js
+++ index.js
@@ -32,86 +32,84 @@ class PreloadPlugin {
       }
     `;
     let linkParams = [];
-    compiler.plugin('compilation', compilation => {
-      compilation.plugin('html-webpack-plugin-before-html-processing', (htmlPluginData, cb) => {
-        if (options.include === undefined || options.include === 'asyncChunks') {
-          try {
-            extractedChunks = compilation.chunks.filter(chunk => !chunk.isInitial());
-          } catch (e) {
-            extractedChunks = compilation.chunks;
-          }
-        } else if (options.include === 'all') {
-            // Async chunks, vendor chunks, normal chunks.
+    compiler.plugin('emit', (compilation, cb) => {
+      if (options.include === undefined || options.include === 'asyncChunks') {
+        try {
+          extractedChunks = compilation.chunks.filter(chunk => !chunk.isInitial());
+        } catch (e) {
           extractedChunks = compilation.chunks;
-        } else if (Array.isArray(options.include)) {
-          // Keep only user specified chunks
-          extractedChunks = compilation
-              .chunks
-              .filter((chunk) => {
-                const chunkName = chunk.name;
-                // Works only for named chunks
-                if (!chunkName) {
-                  return false;
-                }
-                return options.include.indexOf(chunkName) > -1;
-              });
         }
-
-        const publicPath = compilation.outputOptions.publicPath || '';
-
-        flatten(extractedChunks.map(chunk => chunk.files)).filter(entry => {
-          return this.options.fileBlacklist.every(regex => regex.test(entry) === false);
-        }).forEach(entry => {
-          entry = `${publicPath}${entry}`;
-          if (options.rel === 'preload') {
-            let asValue;
-            if (!options.as) {
-              if (entry.match(/\.css$/)) asValue = 'style';
-              else if (entry.match(/\.woff2$/)) asValue = 'font';
-              else asValue = 'script';
-            } else if (typeof options.as === 'function') {
-              asValue = options.as(entry);
-            } else {
-              asValue = options.as;
-            }
-            const crossOrigin = asValue === 'font';
-            linkParams.push({
-              rel: options.rel,
-              as: asValue,
-              crossOrigin: crossOrigin,
-              href: entry
-            });
-          } else {
-            linkParams.push({
-              rel: options.rel,
-              href: entry
-            });
-          }
-        });
-        insertFunction = `
-          (function() {
-            var params = ${JSON.stringify(linkParams)};
-            ${createLinkFunction}
-            setTimeout(function(){
-              for(var i in params){
-                createLink(params[i]);
+      } else if (options.include === 'all') {
+          // Async chunks, vendor chunks, normal chunks.
+        extractedChunks = compilation.chunks;
+      } else if (Array.isArray(options.include)) {
+        // Keep only user specified chunks
+        extractedChunks = compilation
+            .chunks
+            .filter((chunk) => {
+              const chunkName = chunk.name;
+              // Works only for named chunks
+              if (!chunkName) {
+                return false;
               }
-            },${options.delay});
-          })()
-        `;
-        const targetChunkFile = compilation.chunks.find((i) => {return i.name == options.insertChunk;})
-        if (targetChunkFile) {
-          const chunkFile = targetChunkFile.files[0];
-          const targetAssets = compilation.assets[chunkFile];
-          if (targetAssets) {
-            const source = targetAssets.source();
-            targetAssets.source = () => {
-                return source + insertFunction;
+              return options.include.indexOf(chunkName) > -1;
+            });
+      }
+
+      const publicPath = compilation.outputOptions.publicPath || '';
+
+      flatten(extractedChunks.map(chunk => chunk.files)).filter(entry => {
+        return this.options.fileBlacklist.every(regex => regex.test(entry) === false);
+      }).forEach(entry => {
+        entry = `${publicPath}${entry}`;
+        if (options.rel === 'preload') {
+          let asValue;
+          if (!options.as) {
+            if (entry.match(/\.css$/)) asValue = 'style';
+            else if (entry.match(/\.woff2$/)) asValue = 'font';
+            else asValue = 'script';
+          } else if (typeof options.as === 'function') {
+            asValue = options.as(entry);
+          } else {
+            asValue = options.as;
+          }
+          const crossOrigin = asValue === 'font';
+          linkParams.push({
+            rel: options.rel,
+            as: asValue,
+            crossOrigin: crossOrigin,
+            href: entry
+          });
+        } else {
+          linkParams.push({
+            rel: options.rel,
+            href: entry
+          });
+        }
+      });
+      insertFunction = `
+        (function() {
+          var params = ${JSON.stringify(linkParams)};
+          ${createLinkFunction}
+          setTimeout(function(){
+            for(var i in params){
+              createLink(params[i]);
             }
+          },${options.delay});
+        })()
+      `;
+      const targetChunkFile = compilation.chunks.find((i) => {return i.name == options.insertChunk;})
+      if (targetChunkFile) {
+        const chunkFile = targetChunkFile.files[0];
+        const targetAssets = compilation.assets[chunkFile];
+        if (targetAssets) {
+          const source = targetAssets.source();
+          targetAssets.source = () => {
+              return source + insertFunction;
           }
         }
-        cb(null, htmlPluginData);
-      });
+      }
+      cb();
     });
   }
 }
